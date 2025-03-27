a9413685e50f41a3840b9367a71302d1c9c401aa
emilymosq <emilymosq4@gmail.com>
2025-03-27 13:45:39 +0100

    Navegacion en App y creado el componente de Home

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import {NavigationContainer} from "@react-navigation/native";
