900ae85c45c587b6bd0f0156c2dc7bf25c2f1e86
emilymosq <emilymosq4@gmail.com>
2025-03-27 13:42:58 +0100

    Navegacion en App y creado el componente de Home

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {NavigationContainer} from "@react-navigation/native";
+import Home from "./app/presentation/views/home/Home";
+
+
+const Stack= createNativeStackNavigator<RootStackParamlist>();
+
+export type RootStackParamlist = {
+  Home: undefined,
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name={"Home"} component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
