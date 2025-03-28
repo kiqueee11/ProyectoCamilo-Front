e6a3bfb1abb053927167883cf752a32d03478c71
emilymosq <emilymosq4@gmail.com>
2025-03-28 10:46:43 +0100

    Rutas de las vistas en App, comenzando el componente de home

--- app/presentation/views/home/Home.tsx
+++ app/presentation/views/home/Home.tsx
@@ -1,10 +1,19 @@
-import react from 'react';
+import React from 'react';
 import {Text, View} from "react-native";
+import stylesHome from './StylesHome';
+import {Filtro} from "../../components/Filtro";
+import CardEvento from "../../components/CardEvento";
 
-const Home = () =>{
+const Home = () => {
     return(
-        <View>
-            <Text>Home</Text>
+        <View style={stylesHome.container}>
+            <Text style={stylesHome.textPrincipal}>Eventos</Text>
+            <View>
+                <Filtro/>
+            </View>
+            <View>
+                <CardEvento />
+            </View>
         </View>
     )
 }
