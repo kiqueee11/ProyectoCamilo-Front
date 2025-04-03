5cda2acf6b99c14aecb2713f7b2c652ad8f20a15
axelPortatil <axelperez221105@gmail.com>
2025-04-03 11:51:12 +0200

    añadido toast de validaciones en login

--- app/data/repositories/UserRepository.tsx
+++ app/data/repositories/UserRepository.tsx
@@ -1,6 +1,7 @@
 import { ApiDelivery } from "../sources/remote/api/ApiDelivery";
 
 import { AxiosError } from "axios";
+import Toast from "react-native-toast-message";
 import { UserInterface } from "../../domain/entities/User";
 import { UserRepository } from "../../domain/repositories/UserRepository";
 export class UserRepositoryImpl implements UserRepository{
@@ -13,8 +14,13 @@ export class UserRepositoryImpl implements UserRepository{
             });
             return { data: response.data, status: response.status }; // Devuelve los datos y el status
         } catch (error) {
-            const e = error as AxiosError<{ message?: string }>;
+            const e = error as AxiosError<{ error?: string }>;
             console.log("Error: " + JSON.stringify(e.response?.data));
+            Toast.show({
+                type: "error",
+                text1: e.response?.data.error,
+                position: "bottom",
+            })
             throw new Error(e.response?.data?.message || "Error al verificar el usuario");
         }
     }
--- app/presentation/views/auth/Login.tsx
+++ app/presentation/views/auth/Login.tsx
@@ -6,64 +6,68 @@ import FormInput from "../../components/FormInput";
 import {RoundedButton} from "../../components/RoundedButton";
 import {CustomButton} from "../../components/CustomButton";
 import { checkUser } from "../../../domain/useCases/auth/CheckUser";
+import Toast from "react-native-toast-message";
+
 
 export function LoginScreen({navigation, route}: PropsStackNavigation) {
 
-const [email, setEmail] = React.useState("");
-const [password, setPassword] = React.useState("");
-const handleCheckUser = async (email: string, password: string): Promise<void> => {
-    console.log("Email:", email, "Password:", password);
-    const {data, status} = await checkUser(email, password);
-           if (status === 200){
-                console.log("usuario encontrado")
-                navigation.replace("Home")
-           }else{
-                console.log("Error")
-           }
-}
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
 
-return (
-    <View style={styles.container}>
-        <View style={styles.logoContainer}>
-            <Image
-                style={styles.logo}
-                source={require('../../../../assets/logo-app-eventos.png')}
-            ></Image>
-        </View>
-        <Text style={styles.title}>¡Bienvenido! </Text>
-        <Text style={styles.subTitle}>Inicia sesión con tu cuenta</Text>
+    const handleCheckUser = async (email: string, password: string): Promise<void> => {
+        console.log("Email:", email, "Password:", password);
+        const {data, status} = await checkUser(email, password);
+               if (status === 200){
+                    console.log("usuario encontrado")
+                    navigation.replace("Home")
+               }else{
+                    console.log("Error")
+               }
+    }
 
-        <View style={styles.formContainer}>
 
-            <FormInput
-                image={null}
-                text={"Correo eletrónico"}
-                placeholder={""}
-                keyboardType="email-address"
-                secureTextEntry={false}
-                editable={true}
-                onPressFormInterface={(text) => setEmail(text)}
-
-            ></FormInput>
-            <FormInput
-                image={null}
-                text={"Contraseña"}
-                placeholder={""}
-                keyboardType="default"
-                secureTextEntry={true}
-                editable={true}
-                onPressFormInterface={(text) => setPassword(text)}
-
-            ></FormInput>
-            <View style={styles.buttonContainer}>
-                <CustomButton onPress={() => {
-                    handleCheckUser(email, password);
-                }} text={"Entrar"}/>
+    return (
+        <View style={styles.container}>
+            <View style={styles.logoContainer}>
+                <Image
+                    style={styles.logo}
+                    source={require('../../../../assets/logo-app-eventos.png')}
+                ></Image>
             </View>
-        </View>
+            <Text style={styles.title}>¡Bienvenido! </Text>
+            <Text style={styles.subTitle}>Inicia sesión con tu cuenta</Text>
 
-    </View>
-);
+            <View style={styles.formContainer}>
+
+                <FormInput
+                    image={null}
+                    text={"Correo eletrónico"}
+                    placeholder={""}
+                    keyboardType="email-address"
+                    secureTextEntry={false}
+                    editable={true}
+                    onPressFormInterface={(text) => setEmail(text)}
+
+                ></FormInput>
+                <FormInput
+                    image={null}
+                    text={"Contraseña"}
+                    placeholder={""}
+                    keyboardType="default"
+                    secureTextEntry={true}
+                    editable={true}
+                    onPressFormInterface={(text) => setPassword(text)}
+
+                ></FormInput>
+                <View style={styles.buttonContainer}>
+                    <CustomButton onPress={() => {
+                        handleCheckUser(email, password);
+                    }} text={"Entrar"}/>
+                </View>
+            </View>
+        <Toast/>
+        </View>
+    );
 
 
 }
