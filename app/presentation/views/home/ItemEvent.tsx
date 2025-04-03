7d04653c896904cb409f9a79ccb0219b881ac336
axelPortatil <axelperez221105@gmail.com>
2025-04-03 10:33:47 +0200

    añadiendo +2h a dateObj para arreglar problema de fecha

--- app/presentation/views/home/ItemEvent.tsx
+++ app/presentation/views/home/ItemEvent.tsx
@@ -1,7 +1,7 @@
 import {EventInterface} from "../../../domain/entities/Event";
 import CardEvento from "../../components/CardEvento";
 import {TouchableOpacity} from "react-native";
-import React from "react";
+import React, {useEffect} from "react";
 import {useNavigation} from "@react-navigation/native";
 import {NativeStackNavigationProp} from "@react-navigation/native-stack";
 import {RootStackParamlist} from "../../../../App";
@@ -13,11 +13,19 @@ interface Props{
 export const RenderEvent = ({item}: Props) => {
     const navigation = useNavigation<NativeStackNavigationProp<RootStackParamlist>>();
     const dateObj = new Date(item.date);
+    dateObj.setHours(dateObj.getHours() + 2);
     const formattedDate = new Intl.DateTimeFormat("es-ES", {
         day:  "2-digit",
         month: "2-digit",
         year: "numeric"
     }).format(dateObj);
+
+
+    useEffect(() => {
+        console.log("Item date"+item.date)
+        console.log("dateObj"+dateObj)
+        console.log("formattedDate"+formattedDate)
+    }, []);
 
     return(
         <TouchableOpacity onPress={() => {
