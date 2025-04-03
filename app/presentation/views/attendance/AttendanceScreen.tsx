c0691b6fb01e5b93a0bc267f62a92828819ad277
AlexGL00 <153726866+AlexGL00@users.noreply.github.com>
2025-04-03 11:50:00 +0200

    estilo minimalista 50/50

--- app/presentation/views/attendance/AttendanceScreen.tsx
+++ app/presentation/views/attendance/AttendanceScreen.tsx
@@ -34,23 +34,37 @@ const AttendanceScreen = ({navigation}: PropsStackNavigation) => {
         }
     }
 
+    const formatDate = (dateString: string) => {
+        const date = new Date(dateString);
+        return date.toLocaleString("es-ES", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit"
+            // hour: "2-digit",
+            // minute: "2-digit",
+            // hour12: false
+        });
+    };
+
+
     return (
         <View style={stylesAsistencia.container}>
-            <Text style={stylesAsistencia.titulo}>Asistencias</Text>
+            <Text style={stylesAsistencia.titulo}> Asistencias </Text>
             <View style={stylesAsistencia.formContainer}>
-                <Text style={stylesAsistencia.text}> {event.title} </Text>
-                <Text style={stylesAsistencia.textDetails}>{event.date}</Text>
+                <Text style={stylesAsistencia.text}>{event.title} </Text>
+
+                <Text style={stylesAsistencia.textDetails}>{formatDate(event.date)}</Text>
                 <Text style={stylesAsistencia.textDetails}>{event.location}</Text>
-                <Text style={stylesAsistencia.textEvent}>Registrados: {eventStats?.registered} </Text>
-                <Text style={stylesAsistencia.textEvent}>No asisten: {eventStats?.missing}</Text>
-                <Text style={stylesAsistencia.textEvent}>Asistentes: {eventStats?.attenders}</Text>
+
+                <Text style={stylesAsistencia.textEvent}>Registrados: {eventStats?.registered} , No asisten: {eventStats?.missing} , Asistentes: {eventStats?.attenders} </Text>
+
                 <FlatList
                     data={attenders}
                     keyExtractor={(item) => item.id.toString()}
                     style={stylesAsistencia.contentContainer}
                     renderItem={({ item }) => (
                         <View style={stylesAsistencia.listItems}>
-                            <Text style={stylesAsistencia.listText}>- {item.user.name}</Text>
+                            <Text style={stylesAsistencia.listText}>✔️  {item.user.name}   </Text>
                         </View>
                     )}
                 />
